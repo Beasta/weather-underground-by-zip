0b23854e41d3a06a3138da182518a057ed77d318
Beasta <barryblaha@gmail.com>
2017-04-29 12:42:58 -0700

    Cleanup code according to new style rules

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 import * as superagent from 'superagent';
-const ogTitleGet = body => {
-    const regex = /<meta property="og:title" content="([^\|]+?) \| ([0-9]+\.[0-9].+) \| ([^"]+?)" \/>/g;
-    let matches;
-    if ((matches = regex.exec(body)) !== null) {
-        return {
-            location: matches[1],
-            temperature: parseFloat(matches[2]),
-            description: matches[3]
-        }
-    }
-    return {};
+
+const ogTitleGet = (body) => {
+  const regex = /<meta property="og:title" content="([^|]+?) \| ([0-9]+\.[0-9].+) \| ([^"]+?)" \/>/g;
+  const matches = regex.exec(body);
+  if (matches !== null) {
+    return {
+      location: matches[1],
+      temperature: parseFloat(matches[2]),
+      description: matches[3],
+    };
+  }
+  return {};
 };
 module.exports = function WeatherUnderground() {
-    if (!(this instanceof WeatherUnderground)) {
-        return new WeatherUnderground();
+  if (!(this instanceof WeatherUnderground)) {
+    return new WeatherUnderground();
+  }
+  this.request = function (zipcode, callback) {
+    if (!zipcode) {
+      callback('Must enter a zipcode');
+      return;
     }
-    this.request = function (zipcode, callback) {
-        if (!zipcode) {
-            callback('Must enter a zipcode');
-            return;
-        }
-        superagent
-            .get('https://www.wunderground.com/cgi-bin/findweather/getForecast')
-            .query({query: zipcode})
-            .set('Accept', 'application/json')
-            .end(function (err, res) {
-                if (err) {
-                    callback(err);
-                }
-                else {
-                    callback(null, ogTitleGet(res.text));
-                }
-            });
-    }
+    superagent
+        .get('https://www.wunderground.com/cgi-bin/findweather/getForecast')
+        .query({ query: zipcode })
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          if (err) {
+            callback(err);
+          } else {
+            callback(null, ogTitleGet(res.text));
+          }
+        });
+  };
 };
--- test/main.js
+++ test/main.js
@@ -1,36 +1,40 @@
-var WeatherUnderground = require('../build/index.js');
-var assert = require('chai').assert;
-describe('WeatherUnderground', function () {
-	describe('.request', function () {
-		it('should return weather data for a valid zipcode', function (done) {
-			WeatherUnderground().request(92122, function (err, data) {
-			    assert.equal(!!data, true);
-			    done();
-			})
-		});
-        it('should return an error if no zipcode is given', function (done) {
-            WeatherUnderground().request(undefined, function (err, data) {
-                assert.equal(!!err, true);
-                done();
-            })
-        });
-		it('should return a number for the temperature', function (done) {
-			WeatherUnderground().request(92122, function (err, data) {
-				assert.isNumber(data.temperature);
-				done();
-			})
-		});
-		it('should return San Diego for 92122', function (done) {
-			WeatherUnderground().request(92122, function (err, data) {
-				assert.equal(data.location, 'San Diego, CA');
-				done();
-			})
-		})
-        it('should return string for a description', function (done) {
-            WeatherUnderground().request(92122, function (err, data) {
-                assert.isString(data.description);
-                done();
-            })
-        })
-	})
+/* global describe */
+/* global it */
+
+const WeatherUnderground = require('../build/index.js');
+const assert = require('chai').assert;
+
+describe('WeatherUnderground', () => {
+  describe('.request', () => {
+    it('should return weather data for a valid zipcode', (done) => {
+      WeatherUnderground().request(92122, (err, data) => {
+        assert.equal(!!data, true);
+        done();
+      });
+    });
+    it('should return an error if no zipcode is given', (done) => {
+      WeatherUnderground().request(undefined, (err) => {
+        assert.equal(!!err, true);
+        done();
+      });
+    });
+    it('should return a number for the temperature', (done) => {
+      WeatherUnderground().request(92122, (err, data) => {
+        assert.isNumber(data.temperature);
+        done();
+      });
+    });
+    it('should return San Diego for 92122', (done) => {
+      WeatherUnderground().request(92122, (err, data) => {
+        assert.equal(data.location, 'San Diego, CA');
+        done();
+      });
+    });
+    it('should return string for a description', (done) => {
+      WeatherUnderground().request(92122, (err, data) => {
+        assert.isString(data.description);
+        done();
+      });
+    });
+  });
 });
